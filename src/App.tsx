a03a835be9db63c62b6df6b04b279a61464a6ff3
Victor França <vito.daleste@gmail.com>
2022-12-05 12:41:17 -0300

    feat: integração back-end com front-end

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { Footer } from './components/estaticos/Footer/Footer';
 import { Navbar } from './components/estaticos/Navbar/navbar';
 import { Home } from './paginas/home/Home';
+import { Login } from './paginas/login/Login';
+import { CadastroUsuario } from './paginas/cadastroUsuario/CadastroUsuario';
 
 
 function App() {
@@ -11,7 +13,10 @@ function App() {
     <Router>
       <Navbar />
       <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/login" element={<Login />} />
         <Route path="/home" element={<Home />} />
+        <Route path="/cadastrousuario" element={<CadastroUsuario />} />
       </Routes>
       <Footer />
     </Router>
