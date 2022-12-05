697b5338303b6079d512019e8ca6aa7197b01c71
Victor França <vito.daleste@gmail.com>
2022-12-05 12:33:53 -0300

    fix: refatoramento da navbar

--- src/components/estaticos/Navbar/navbar.tsx
+++ src/components/estaticos/Navbar/navbar.tsx
@@ -6,6 +6,7 @@ import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import { Button, IconButton } from "@mui/material";
 import MenuIcon from '@mui/icons-material/Menu';
+import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
@@ -19,10 +20,9 @@ function Navbar() {
           aria-label="menu"
           sx={{ mr: 2 }}
         >
-          <MenuIcon />
         </IconButton>
 
-        <Box style={{ cursor: "pointer" }} >
+        <Box className='cursor' >
 
           <Typography variant="h5" color="inherit">
             BlogPessoal
@@ -30,31 +30,33 @@ function Navbar() {
         </Box>
 
         <Box display="flex" justifyContent="start">
-          <Box mx={1} style={{ cursor: "pointer" }}>
+          <Box mx={1} className='cursor'>
             <Typography variant="h6" color="inherit">
               <Button color="inherit">Home</Button>
             </Typography>
           </Box>
-          <Box mx={1} style={{ cursor: "pointer" }}>
+          <Box mx={1} className='cursor'>
             <Typography variant="h6" color="inherit">
               <Button color="inherit">Postagens</Button>
             </Typography>
           </Box>
-          <Box mx={1} style={{ cursor: "pointer" }}>
+          <Box mx={1} className='cursor'>
             <Typography variant="h6" color="inherit">
               <Button color="inherit">Temas</Button>
             </Typography>
           </Box>
-          <Box mx={1} style={{ cursor: "pointer" }}>
+          <Box mx={1} className='cursor'>
             <Typography variant="h6" color="inherit">
               <Button color="inherit">Cadastrar Temas</Button>
             </Typography>
           </Box>
-          <Box mx={1} style={{ cursor: "pointer" }}>
-            <Typography variant="h6" color="inherit">
-              <Button color="inherit">Logout</Button>
-            </Typography>
-          </Box>
+          <Link to='/login' className="text-decorator-none">
+            <Box mx={1} style={{ cursor: "pointer", color: 'white' }}>
+              <Typography variant="h6" color="inherit">
+                <Button color="inherit">Logout</Button>
+              </Typography>
+            </Box>
+          </Link>
         </Box>
 
       </Toolbar>
